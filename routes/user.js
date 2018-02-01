79e848ba2277f0af63318c9d356e8020892d07a8
imnotthebatman <noe_lia_lo@hotmail.com>
2018-02-01 17:14:58 +0100

    cambios

--- routes/user.js
+++ routes/user.js
@@ -8,8 +8,13 @@ var UserController = require("../controllers/user");
 //el router y ya podemos crear rutas
 var api  = express.Router();
 
+//cargar middleware en objeto 
+var md_auth = require("../middlewares/authenticated");
+
 //¿cómo crear una ruta? api.get('ruta',metodo a cargar);
-api.get('/probando-controlador', UserController.pruebas);
+//api.get('/probando-controlador', UserController.pruebas);
+//para usarlo como middleware segundo parámetro 
+api.get('/probando-controlador',md_auth.ensureAuth, UserController.pruebas);
 //prueba post
 api.post('/register', UserController.saveUser);
 //login por post
